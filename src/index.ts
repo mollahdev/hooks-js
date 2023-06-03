37b54b52ed9bddda9b1e234f7dec3c11d8a97f6e
Ashraf Mollah <mollah.dev@gmail.com>
2023-06-03 08:33:29 +0600

    extract hooks methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-// import CreateHook from "@/createHook"
+import CreateHook from "@/createHook"
 
-// const hook = new CreateHook();
+const hook = new CreateHook();
+const addAction = hook.addAction.bind(hook)
+const doAction = hook.doAction.bind(hook)
+const removeAction = hook.removeAction.bind(hook)
+const addFilter = hook.addFilter.bind(hook)
+const applyFilters = hook.applyFilters.bind(hook)
+const removeFilter = hook.removeFilter.bind(hook)
+
+export {
+    addAction,
+    doAction,
+    removeAction,
+    addFilter,
+    applyFilters,
+    removeFilter
+}
+
+export default hook;
